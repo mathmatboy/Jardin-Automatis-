06e07bc108704fc32f4db3a3263374ab66256548
mathmatboy <math1242@hotmail.com>
2020-05-30 21:43:56 -0400

    V1.1
    Correction de la pompe a eau

--- jardinautov1.1.ino
+++ jardinautov1.1.ino
@@ -4,19 +4,21 @@
 int analogPinTomate = A0;
 int analogTomateValue;
 int limitTomate = 500;
+bool etatValveTomate = false;
 /*
  * FINE HERBE
  */
 int analogPinFineHerbe = A1;
 int analogFineHerbeValue;
 int limitFineHerbe = 450;
+bool etatValveFineHerbe = false;
 
 void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);//VALVE TOMATE
   pinMode(12, OUTPUT);//VALVE FINE HERBE
 
-  pinMode(11, OUTPUT);
+  pinMode(8, OUTPUT);//POMPE
 }
 
 void loop() {
@@ -29,16 +31,15 @@ void loop() {
   Serial.println("Analog TOMATE valeur : ");
   Serial.println(analogTomateValue);
   if (analogTomateValue<limitTomate) {
-  digitalWrite(13, LOW);
-  digitalWrite(11, LOW);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  etatValveTomate = true;
   Serial.println("Le taux d'humidité des TOMATE est *HAUTE* donc pas besoin d'arroser !");
   }
   else {
-  digitalWrite(13, HIGH);
+  digitalWrite(13, LOW);
   Serial.println("Le taux d'humidité des TOMATE est *BAS*, la vanne ouvre !");
-  delay(1000);
-  digitalWrite(11, HIGH);
-  Serial.println("** La pompe Start T **");
+  etatValveTomate = false;
   }
 
   /*
@@ -47,16 +48,26 @@ void loop() {
   Serial.println("Analog Fine Herbe valeur : ");
   Serial.println(analogFineHerbeValue);
   if (analogFineHerbeValue<limitFineHerbe) {
-  digitalWrite(12, LOW);
-  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay(1000);
+  etatValveFineHerbe = true;
   Serial.println("Le taux d'humidité des FINE HERBE est *HAUTE* donc pas besoin d'arroser !");
   }
   else {
-  digitalWrite(12, HIGH);
+  digitalWrite(12, LOW);
   Serial.println("Le taux d'humidité des FINE HERBE est *BAS*, la vanne ouvre !");
-  delay(1000);
-  digitalWrite(11, HIGH);
-  Serial.println("** La pompe Start FH **");
+  etatValveFineHerbe = false;
   }
-  delay(2000); 
+
+  if(etatValveTomate || etatValveFineHerbe){
+    digitalWrite(8, HIGH);
+    Serial.println("** La pompe Démarre **");
+  }
+  else{
+    digitalWrite(8, LOW);
+    Serial.println("** La pompe en Arrêt **");
+  }
+  delay(2000);
+  
+   
 }
